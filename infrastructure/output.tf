2d42937bd3a83755c19a9b929eb5f624c1145980
Jonathan <jonathan@edumarx.co.za>
2021-02-03 11:25:28 +0200

    Coding

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,4 +1,10 @@
 #Add/remove outputs according to infrastructure specified in main (e.g. if a stepfunction is removed, the output for it should also be removed).
-output "output" {
+output "glue_job" {
 
+    value = module.glue_ingest_default_job
+}
+
+output "step_function" {
+
+    value = module.step_1_parallelism
 }
